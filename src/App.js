c66a1df4f7eab681ff4c5bebedfce29cc82ece2a
marziyehadineh <MarziyehAdineh1.gmail.com>
2021-11-11 18:35:51 +0330

    modalContainer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import Modal from "./Components/Modal.js";
-
 import "./App.css";
 function App() {
-  const [openModal, setOpenModal] = useState(false);
+  const [ openModal, setOpenModal] = useState(false);
   return (
     <div className="App">
       <h1>سلام، روی دکمه کلیک کنید تا مدل باز شود</h1>
-      <button className="openModalBtn" onClick={() => setOpenModal(true)}>
+      <button
+       className="openModalBtn"
+       onClick={() => setOpenModal(true)}>
         باز
       </button>
       {openModal && < Modal closeModal={setOpenModal}/>}
